c43cf14e0b99120aaea1f4bf5957409e5f6c1d63
Alex Parker <alexander_parker@me.com>
2017-12-07 23:16:04 -0500

    iframe sizing for mobile

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
 				</div>
 				<div class="row">
 					<div class="twelve columns">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111751.09041404405!2d-82.50966963758829!3d28.921471345579352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e872780907ed6f%3A0x1769547fe250722b!2sCalvary+Church!5e0!3m2!1sen!2sus!4v1512621362448" width="604" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+						<iframe class="u-full-width" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111751.09041404405!2d-82.50966963758829!3d28.921471345579352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e872780907ed6f%3A0x1769547fe250722b!2sCalvary+Church!5e0!3m2!1sen!2sus!4v1512621362448" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
 					</div>
 				</div>
 			</div>
